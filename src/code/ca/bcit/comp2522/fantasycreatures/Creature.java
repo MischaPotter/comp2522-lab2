a19905a7de3f6a9219e9ba50d58d51aa813bf1ea
Mischa Potter <mischapotter@icloud.com>
2026-01-22 14:23:39 -0800

    Added base classes and class level javadoc comments

---
+++ src/code/ca/bcit/comp2522/fantasycreatures/Creature.java
@@ -0,0 +1,12 @@
+package ca.bcit.comp2522.fantasycreatures;
+
+/**
+ *
+ *
+ * @author Mischa Potter
+ * @author Abdullah Munawar
+ * @version 1.0
+ */
+public class Creature
+{
+}
---
+++ src/code/ca/bcit/comp2522/fantasycreatures/Elf.java
@@ -0,0 +1,13 @@
+package ca.bcit.comp2522.fantasycreatures;
+
+/**
+ *
+ *
+ * @author Mischa Potter
+ * @author Abdullah Munawar
+ * @version 1.0
+ */
+
+public class Elf
+{
+}
